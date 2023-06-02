b85ef5ce8f92b7d7400b19703a8296fc368dfad1
Ammi González <64441015+AmmiGonzalez@users.noreply.github.com>
2023-06-02 08:46:05 -0600

    Finalización de Costo Mínimo

--- ProyectoFinalIO/CostoMinimo.Designer.cs
+++ ProyectoFinalIO/CostoMinimo.Designer.cs
@@ -29,32 +29,186 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.dgvEcuaciones = new System.Windows.Forms.DataGridView();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblOferta = new System.Windows.Forms.Label();
+            this.lblPlantas = new System.Windows.Forms.Label();
+            this.txtbX1 = new System.Windows.Forms.TextBox();
+            this.lblX = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblMoneda = new System.Windows.Forms.Label();
+            this.txtMoneda = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTransporte = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEcuaciones)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(187, 92);
+            this.button1.Location = new System.Drawing.Point(693, 484);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 59);
+            this.button1.Size = new System.Drawing.Size(139, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Correr test de calculo";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(15, 464);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(104, 24);
+            this.lblResultado.TabIndex = 61;
+            this.lblResultado.Text = "Resultado: ";
+            // 
+            // dgvEcuaciones
+            // 
+            this.dgvEcuaciones.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dgvEcuaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEcuaciones.Location = new System.Drawing.Point(15, 192);
+            this.dgvEcuaciones.Name = "dgvEcuaciones";
+            this.dgvEcuaciones.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dgvEcuaciones.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvEcuaciones.Size = new System.Drawing.Size(816, 209);
+            this.dgvEcuaciones.TabIndex = 60;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(15, 416);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(200, 29);
+            this.btnCalcular.TabIndex = 59;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblOferta
+            // 
+            this.lblOferta.AutoSize = true;
+            this.lblOferta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOferta.Location = new System.Drawing.Point(12, 155);
+            this.lblOferta.Name = "lblOferta";
+            this.lblOferta.Size = new System.Drawing.Size(398, 24);
+            this.lblOferta.TabIndex = 58;
+            this.lblOferta.Text = "Ingrese los datos de costos, oferta y demanda:";
+            // 
+            // lblPlantas
+            // 
+            this.lblPlantas.AutoSize = true;
+            this.lblPlantas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlantas.Location = new System.Drawing.Point(12, 9);
+            this.lblPlantas.Name = "lblPlantas";
+            this.lblPlantas.Size = new System.Drawing.Size(229, 24);
+            this.lblPlantas.TabIndex = 54;
+            this.lblPlantas.Text = "Ingrese los datos iniciales:";
+            // 
+            // txtbX1
+            // 
+            this.txtbX1.Location = new System.Drawing.Point(227, 47);
+            this.txtbX1.Name = "txtbX1";
+            this.txtbX1.Size = new System.Drawing.Size(314, 20);
+            this.txtbX1.TabIndex = 48;
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX.Location = new System.Drawing.Point(12, 47);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(191, 24);
+            this.lblX.TabIndex = 47;
+            this.lblX.Text = "Cantidad de plantas =";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(547, 115);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(284, 29);
+            this.btnAceptar.TabIndex = 62;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lblMoneda
+            // 
+            this.lblMoneda.AutoSize = true;
+            this.lblMoneda.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMoneda.Location = new System.Drawing.Point(12, 82);
+            this.lblMoneda.Name = "lblMoneda";
+            this.lblMoneda.Size = new System.Drawing.Size(168, 24);
+            this.lblMoneda.TabIndex = 63;
+            this.lblMoneda.Text = "Moneda utilizada =";
+            // 
+            // txtMoneda
+            // 
+            this.txtMoneda.Location = new System.Drawing.Point(227, 87);
+            this.txtMoneda.Name = "txtMoneda";
+            this.txtMoneda.Size = new System.Drawing.Size(314, 20);
+            this.txtMoneda.TabIndex = 64;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 24);
+            this.label1.TabIndex = 65;
+            this.label1.Text = "Unidad de Transporte =";
+            // 
+            // txtTransporte
+            // 
+            this.txtTransporte.Location = new System.Drawing.Point(227, 124);
+            this.txtTransporte.Name = "txtTransporte";
+            this.txtTransporte.Size = new System.Drawing.Size(314, 20);
+            this.txtTransporte.TabIndex = 66;
+            // 
             // CostoMinimo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(843, 533);
+            this.Controls.Add(this.txtTransporte);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMoneda);
+            this.Controls.Add(this.lblMoneda);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.dgvEcuaciones);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblOferta);
+            this.Controls.Add(this.lblPlantas);
+            this.Controls.Add(this.txtbX1);
+            this.Controls.Add(this.lblX);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "CostoMinimo";
             this.Text = "CostoMinimo";
+            this.Load += new System.EventHandler(this.CostoMinimo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEcuaciones)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.DataGridView dgvEcuaciones;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblOferta;
+        private System.Windows.Forms.Label lblPlantas;
+        private System.Windows.Forms.TextBox txtbX1;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblMoneda;
+        private System.Windows.Forms.TextBox txtMoneda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTransporte;
     }
 }
--- ProyectoFinalIO/CostoMinimo.cs
+++ ProyectoFinalIO/CostoMinimo.cs
@@ -14,6 +14,13 @@ namespace ProyectoFinalIO
     public partial class CostoMinimo : Form
     {
         CostoMinimoClass obj = new CostoMinimoClass();
+        private List<decimal> demandas;
+        private List<decimal> ofertas;
+        private List<List<decimal>> costos;
+        private List<string> fuentes;
+        private List<string> destinos;
+        private string unidadesTransporte;
+        private string monedaUtilizada;
 
         public CostoMinimo()
         {
@@ -39,6 +46,135 @@ namespace ProyectoFinalIO
             string monedaUtilizada = "Q";
 
             MessageBox.Show(obj.calcular(costos, fuentes, ofertas, destinos, demandas, unidadesTransporte, monedaUtilizada));
+        }
+
+        private void CostoMinimo_Load(object sender, EventArgs e)
+        {
+            lblOferta.Visible = false;
+            dgvEcuaciones.Visible = false;
+            btnCalcular.Visible = false;
+            lblResultado.Visible = false;
+            button1.Visible = false;
+            lblResultado.Visible = false;
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            int cant = Convert.ToInt16(txtbX1.Text) + 1;
+            lblOferta.Visible = true;
+            dgvEcuaciones.Visible = true;
+            btnCalcular.Visible = true;
+            lblResultado.Visible = true;
+
+            fuentes = new List<string>(); // Declarar la lista fuera del bucle
+            destinos = new List<string>();
+            int filas = cant + 1; 
+            int columnas = cant + 1;
+
+            dgvEcuaciones.AllowUserToAddRows = false; // Desactiva la opción de agregar filas manualmente
+            dgvEcuaciones.RowCount = filas; // Establece el número de filas del DataGridView
+            dgvEcuaciones.ColumnCount = columnas; // Establece el número de columnas del DataGridView
+
+            for (int i = 0; i < cant; i++)
+            {
+                fuentes.Add("planta " + (i + 1)); // Agregar elementos a la lista
+                destinos.Add("destino " + (i + 1));
+            }
+            // Asigna los valores de los destinos en la primera fila
+            for (int j = 1; j < columnas; j++)
+            {
+                dgvEcuaciones.Rows[0].Cells[j].Value = destinos[j - 1];
+            }
+
+            // Agrega "oferta" en la última celda de la primera fila
+            dgvEcuaciones.Rows[0].Cells[columnas - 1].Value = "oferta";
+
+            // Asigna los valores de las plantas en la misma columna (columna 0)
+            for (int i = 0; i < cant; i++)
+            {
+                fuentes.Add("planta " + (i + 1));
+                dgvEcuaciones.Rows[i + 1].Cells[0].Value = fuentes[i]; // Asigna el valor de la planta en la celda correspondiente
+            }
+
+            // Agrega "demanda" en la última celda de la primera columna
+            dgvEcuaciones.Rows[filas - 1].Cells[0].Value = "demanda";
+
+            unidadesTransporte = txtTransporte.Text;
+            monedaUtilizada = txtMoneda.Text;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // Obtener los datos del DataGridView y las listas
+            demandas = new List<decimal>();
+            ofertas = new List<decimal>();
+            costos = new List<List<decimal>>();
+            fuentes = new List<string>();
+            destinos = new List<string>();
+
+            // Recorrer el DataGridView para obtener los valores
+            for (int i = 1; i < dgvEcuaciones.Rows.Count - 1; i++)
+            {
+                DataGridViewRow row = dgvEcuaciones.Rows[i];
+                List<decimal> fila = new List<decimal>();
+
+                for (int j = 1; j < row.Cells.Count - 1; j++)
+                {
+                    DataGridViewCell cell = row.Cells[j];
+                    fila.Add(Convert.ToDecimal(cell.Value));
+                }
+
+                costos.Add(fila);
+            }
+
+            for (int j = 1; j < dgvEcuaciones.Columns.Count - 1; j++)
+            {
+                DataGridViewCell cell = dgvEcuaciones.Rows[dgvEcuaciones.Rows.Count - 1].Cells[j];
+                demandas.Add(Convert.ToDecimal(cell.Value));
+            }
+
+            for (int j = 1; j < dgvEcuaciones.Columns.Count - 1; j++)
+            {
+                DataGridViewCell cell = dgvEcuaciones.Rows[0].Cells[j];
+                fuentes.Add(cell.Value + "");
+            }
+
+            for (int i = 1; i < dgvEcuaciones.Rows.Count - 1; i++)
+            {
+                DataGridViewCell cell = dgvEcuaciones.Rows[i].Cells[dgvEcuaciones.Columns.Count - 1];
+                ofertas.Add(Convert.ToDecimal (cell.Value));
+            }
+
+            for (int i = 1; i < dgvEcuaciones.Rows.Count - 1; i++)
+            {
+                DataGridViewCell cell = dgvEcuaciones.Rows[i].Cells[0];
+                destinos.Add(cell.Value + "");
+            }
+
+            unidadesTransporte = txtTransporte.Text;
+            monedaUtilizada = txtMoneda.Text;
+
+
+            string msg = "";
+
+            for (int fila = 0; fila < 1; fila++)
+            {
+                for (int columna = 0; columna < fuentes.Count; columna++)
+                {
+                    msg += fuentes[columna] + "\t";
+                }
+
+                msg += "\n";
+            }
+
+            MessageBox.Show(msg);
+
+            // Realizar los cálculos utilizando los datos obtenidos
+            string resultado = obj.calcular(costos, fuentes, ofertas, destinos, demandas, unidadesTransporte, monedaUtilizada);
+
+            // Mostrar el resultado en un MessageBox
+            MessageBox.Show(resultado);
+
         }
     }
 }
